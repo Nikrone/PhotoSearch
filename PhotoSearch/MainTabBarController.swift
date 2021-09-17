1547d5b33548b055aa53ea24ac636586715f3c79
Evgeniy Nosko <77491382+Nikrone@users.noreply.github.com>
2021-09-17 19:17:07 +0300

    feat: initial MainTabBarController

---
+++ PhotoSearch/MainTabBarController.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabBarController.swift
+//  PhotoSearch
+//
+//  Created by Evgeniy Nosko on 17.09.21.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+    
+}
