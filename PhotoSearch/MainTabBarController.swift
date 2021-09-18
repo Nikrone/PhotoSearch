0182c7f2b027840bb1b3e3f94bb81537068d9a97
Evgeniy Nosko <77491382+Nikrone@users.noreply.github.com>
2021-09-18 16:44:15 +0300

    add Assets, NavigationItemsAction, UICollectionViewDataSource, UICollectionViewDelegate

--- PhotoSearch/MainTabBarController.swift
+++ PhotoSearch/MainTabBarController.swift
@@ -14,7 +14,21 @@ class MainTabBarController: UITabBarController {
         
         view.backgroundColor = .gray
         
+        let photosVC = PhotosCollectionViewController(collectionViewLayout: UICollectionViewFlowLayout())
         
+        viewControllers = [
+            generateNavigationController(rootViewController: photosVC, title: "Photos", image: #imageLiteral(resourceName: "photos")),
+            generateNavigationController(rootViewController: ViewController(), title: "Favourites", image: #imageLiteral(resourceName: "heart"))
+        ]
+    }
+    
+    private func generateNavigationController(rootViewController: UIViewController, title: String, image: UIImage) -> UIViewController {
+        
+        let navigationVC = UINavigationController(rootViewController: rootViewController)
+        navigationVC.tabBarItem.title = title
+        navigationVC.tabBarItem.image = image
+        
+        return navigationVC
     }
     
 }
--- PhotoSearch/PhotosCollectionViewController.swift
+++ PhotoSearch/PhotosCollectionViewController.swift
@@ -6,3 +6,70 @@
 //
 
 import UIKit
+
+class PhotosCollectionViewController: UICollectionViewController {
+    
+    private lazy var addBarButtonItem: UIBarButtonItem = {
+        return UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addBarButtonTapped))
+    }()
+    
+    private lazy var actionBarButtonItem: UIBarButtonItem = {
+        return UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(actionBarButtonTapped))
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.backgroundColor = .orange
+        setupCollectionView()
+        setupNavigationBar()
+        setupSearchBar()
+    }
+    
+    // MARK: - NavigationItemsAction
+    
+    @objc private func addBarButtonTapped() {
+        print(#function)
+    }
+    
+    @objc private func actionBarButtonTapped() {
+        print(#function)
+    }
+    
+    // MARK: - Setup UI Elements
+    
+//    регистрация ячейки
+    private func setupCollectionView() {
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "CellId")
+    }
+    
+    private func setupNavigationBar() {
+        let titleLabel = UILabel()
+        titleLabel.text = "PHOTOS"
+        titleLabel.font = UIFont.systemFont(ofSize: 15, weight: .medium)
+        titleLabel.textColor = #colorLiteral(red: 0.5019607843, green: 0.4980392157, blue: 0.4980392157, alpha: 1)
+        navigationItem.leftBarButtonItem = UIBarButtonItem.init(customView: titleLabel)
+        
+        navigationItem.rightBarButtonItems = [actionBarButtonItem, addBarButtonItem]
+    }
+    
+    private func setupSearchBar() {
+        let searchController = UISearchController(searchResultsController: nil)
+        navigationItem.searchController = searchController
+    }
+    
+    // MARK: - UICollectionViewDataSource, UICollectionViewDelegate
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CellId", for: indexPath)
+        cell .backgroundColor = .red
+        
+        return cell
+    }
+    
+    
+}
