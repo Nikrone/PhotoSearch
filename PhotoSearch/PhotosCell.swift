aadcec690eaa8bb7c4ab0070d812bfc34ddab080
Evgeniy Nosko <77491382+Nikrone@users.noreply.github.com>
2021-09-19 15:58:22 +0300

    feat: add implementation PhotoCell

--- PhotoSearch/PhotosCell.swift
+++ PhotoSearch/PhotosCell.swift
@@ -11,15 +11,70 @@ class PhotosCell: UICollectionViewCell {
     
     static let reused = "PhotosCell"
     
-    private let photoImageView: UIImageView = {
-        let imageView = UIImageView()
-//        чтобы фиксировать ячейки с помощю кода
+    private let checkmark: UIImageView = {
+        let image = UIImage(named: "checkmark")
+        let imageView = UIImageView(image: image)
+        //        чтобы фиксировать ячейки с помощю кода
         imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.alpha = 0
         return imageView
     }()
     
+    private let photoImageView: UIImageView = {
+        let imageView = UIImageView()
+        //        чтобы фиксировать ячейки с помощю кода
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.backgroundColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    var unsplashPhoto: UnsplashPhoto! {
+        didSet {
+            let photoUrl = unsplashPhoto.urls["regular"]
+//            guard let imageUrl, let url = URL(string: imageUrl) else { return }
+        }
+    }
+    
+    override var isSelected: Bool {
+        didSet {
+            updateSelectedState()
+        }
+    }
+    
+    
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        photoImageView.image = nil
+    }
+    
+    private func updateSelectedState() {
+        photoImageView.alpha = isSelected ? 0.7 : 1
+        checkmark.alpha = isSelected ? 1 : 0
+    }
+    
     override init (frame: CGRect) {
         super.init(frame: frame)
+        
+        setupPhotoImageView()
+        setupCheckmarkView()
+        updateSelectedState()
+    }
+    
+    private func setupPhotoImageView() {
+        addSubview(photoImageView)
+        photoImageView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        photoImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        photoImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        photoImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        
+    }
+    
+    private func setupCheckmarkView() {
+        addSubview(checkmark)
+        checkmark.trailingAnchor.constraint(equalTo: photoImageView.trailingAnchor, constant: -8).isActive = true
+        checkmark.bottomAnchor.constraint(equalTo: photoImageView.bottomAnchor, constant: -8).isActive = true
     }
     
     required init?(coder: NSCoder) {
--- PhotoSearch/PhotosCollectionViewController.swift
+++ PhotoSearch/PhotosCollectionViewController.swift
@@ -12,6 +12,8 @@ class PhotosCollectionViewController: UICollectionViewController {
     var networkDataFetcher = NetworkDataFetcher()
     private var timer: Timer?
     
+    private var photos = [UnsplashPhoto]()
+    
     private lazy var addBarButtonItem: UIBarButtonItem = {
         return UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addBarButtonTapped))
     }()
@@ -65,11 +67,13 @@ class PhotosCollectionViewController: UICollectionViewController {
     // MARK: - UICollectionViewDataSource, UICollectionViewDelegate
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return photos.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CellId", for: indexPath)
+        let unsplashPhoto = photos[indexPath.item]
+        cell
         cell.backgroundColor = .purple
         return cell
     }
@@ -83,10 +87,9 @@ extension PhotosCollectionViewController: UISearchBarDelegate {
         print(searchText)
         
         timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false, block: { (_) in
-            self.networkDataFetcher.fetchImages(searchTerm: searchText) { (searchResults) in
-                searchResults?.results.map({ (photo) in
-                    print(photo.urls["small"])
-                })
+            self.networkDataFetcher.fetchImages(searchTerm: searchText) { [weak self](searchResults) in
+                guard let fetchedPhotos = searchResults else {return}
+                self?.photos = fetchedPhotos.results
             }
         })
         
